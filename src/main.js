e8c819e9e5594526a1dc5bbb60712d0d0929f389
Aditya Oberai <adityaoberai1@gmail.com>
2023-11-14 17:59:38 +0000

    Fix async call

--- src/main.js
+++ src/main.js
@@ -1,16 +1,19 @@
 import { getStaticFile } from './utils.js';
 import { getPdf } from './pdf.js';
 
-export default async ({ req, res }) => {
+export default async ({ req, res, log }) => {
   
   if (req.method === 'GET' && req.path === '/') {
+    log('View website');
     return res.send(getStaticFile('resume.html'), 200, {
       'Content-Type': 'text/html; charset=utf-8',
     });
   }
 
   if (req.method === 'GET' && req.path === '/download') {
-    return res.send(getPdf(getStaticFile('resume.html')), 200, {
+    log('Start download');
+    var pdf = await getPdf(getStaticFile('resume.html'));
+    return res.send(pdf, 200, {
       'Content-Type': 'application/pdf',
     });
   }

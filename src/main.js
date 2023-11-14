2d410f9f3b40055a271298e9f8217704a1a6e384
Aditya Oberai <adityaoberai1@gmail.com>
2023-11-14 17:48:54 +0000

    Add pdf download

--- src/main.js
+++ src/main.js
@@ -1,10 +1,17 @@
 import { getStaticFile } from './utils.js';
+import { getPdf } from './pdf.js';
 
 export default async ({ req, res }) => {
   
-  if (req.method === 'GET') {
+  if (req.method === 'GET' && req.path === '/') {
     return res.send(getStaticFile('resume.html'), 200, {
       'Content-Type': 'text/html; charset=utf-8',
+    });
+  }
+
+  if (req.method === 'GET' && req.path === '/download') {
+    return res.send(getPdf(getStaticFile('resume.html')), 200, {
+      'Content-Type': 'application/pdf',
     });
   }
 };

ca6f633b70f646f06158b69768ab3cc9cd1ebf90
Aditya Oberai <adityaoberai1@gmail.com>
2023-11-14 13:45:34 +0000

    Minor cleanup

--- src/main.js
+++ src/main.js
@@ -2,13 +2,13 @@ import path from 'path';
 import { fileURLToPath } from 'url';
 import fs from 'fs';
 
-export default async ({ req, res, log, error }) => {
+export default async ({ req, res }) => {
   
   if (req.method === 'GET') {
     var filename = fileURLToPath(import.meta.url);
     var dirname = path.dirname(filename);
     var staticFolder = path.join(dirname, '../static');
-    var resume = fs.readFileSync(path.join(staticFolder, 'resume.html')).toString()
+    var resume = fs.readFileSync(path.join(staticFolder, 'resume.html')).toString();
 
     return res.send(resume, 200, {
       'Content-Type': 'text/html; charset=utf-8',

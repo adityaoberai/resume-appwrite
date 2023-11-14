1c37953836475a63002b36fda1d21f528322368a
Aditya Oberai <adityaoberai1@gmail.com>
2023-11-14 13:39:39 +0000

    Add resume

--- src/main.js
+++ src/main.js
@@ -1,33 +1,17 @@
-import { Client } from 'node-appwrite';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import fs from 'fs';
 
-// This is your Appwrite function
-// It's executed each time we get a request
 export default async ({ req, res, log, error }) => {
-  // Why not try the Appwrite SDK?
-  //
-  // const client = new Client()
-  //    .setEndpoint('https://cloud.appwrite.io/v1')
-  //    .setProject(process.env.APPWRITE_FUNCTION_PROJECT_ID)
-  //    .setKey(process.env.APPWRITE_API_KEY);
-
-  // You can log messages to the console
-  log('Hello, Logs!');
-
-  // If something goes wrong, log an error
-  error('Hello, Errors!');
-
-  // The `req` object contains the request data
+  
   if (req.method === 'GET') {
-    // Send a response with the res object helpers
-    // `res.send()` dispatches a string back to the client
-    return res.send('Hello, World!');
-  }
+    var filename = fileURLToPath(import.meta.url);
+    var dirname = path.dirname(filename);
+    var staticFolder = path.join(dirname, '../static');
+    var resume = fs.readFileSync(path.join(staticFolder, 'resume.html')).toString()
 
-  // `res.json()` is a handy helper for sending JSON
-  return res.json({
-    motto: 'Build like a team of hundreds_',
-    learn: 'https://appwrite.io/docs',
-    connect: 'https://appwrite.io/discord',
-    getInspired: 'https://builtwith.appwrite.io',
-  });
+    return res.send(resume, 200, {
+      'Content-Type': 'text/html; charset=utf-8',
+    });
+  }
 };

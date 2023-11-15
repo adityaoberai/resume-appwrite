de12ec660e0c97e267aff37e0fd47bfc77b2e3f1
Aditya Oberai <adityaoberai1@gmail.com>
2023-11-15 06:45:23 +0000

    change request method for download

--- src/main.js
+++ src/main.js
@@ -10,10 +10,16 @@ export default async ({ req, res, log }) => {
     });
   }
 
-  if (req.method === 'GET' && req.path === '/download') {
+  if (req.method === 'POST' && req.path === '/download') {
     log('Start download');
     var pdf = await getPdf(getStaticFile('resume.html'));
     return res.send(pdf, 200, {
+      'Content-Type': 'application/pdf',
+    });
+  }
+
+  if (req.method === 'GET' && req.path === '/download') {
+    return res.send('<h1>Send POST Request to download PDF</h1>', 200, {
       'Content-Type': 'application/pdf',
     });
   }
